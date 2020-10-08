cfa3884f332303618007c664d95a13ade6ff5631
Anna Bullard <annabullard89@gmail.com>
2020-10-08 11:14:43 -0700

    Click Targets events

--- connect-four.js
+++ connect-four.js
@@ -6,31 +6,58 @@ let game = undefined;
 window.addEventListener('DOMContentLoaded', ev=>{
     const player1 = document.getElementById('player-1-name');
     const player2 = document.getElementById('player-2-name');
+    const newGame = document.getElementById('new-game');
 
 
     let boardHolder = document.getElementById('board-holder');
     const gameName = document.getElementById('game-name');
+    const clickTargets = document.getElementById('click-targets');
+
+    const showPlayerColor = (player, target) => {
+        if (player == 1) {
+            target.setAttribute("class","click-target black")
+        } else {
+            target.setAttribute("class","click-target red");
+        }
+    }
 
     let updateUI = () =>{
         if (game == undefined) {
             boardHolder.classList.add('is-invisible');
         } else {
             boardHolder.classList.remove('is-invisible');
-            gameName.innerHTML = `${game.gameName()}`
+            gameName.innerHTML = `${game.getName()}`
+            let currentPlayer = game.firstPlayer;
         }
     }
-    const newGame = getElementById('new-game');
 
-         const checkPlayerStatus = () =>{
-            if(player1.value !== '' && player2.value !== '') {
-                newGame.removeAttribute('disabled');
-            } else {
-                newGame.setAttribute('disabled','true');
-            }
+    const checkPlayerStatus = () =>{
+        if(player1.value !== '' && player2.value !== '') {
+            newGame.removeAttribute('disabled');
+        } else {
+            newGame.setAttribute('disabled','true');
         }
+    }
 
     player1.addEventListener('keyup',checkPlayerStatus);
     player2.addEventListener('keyup',checkPlayerStatus);
+
+    clickTargets.addEventListener("click", ev => {
+        game.playInColumn();
+        updateUI();
+    })
+
+    clickTargets.addEventListener("mouseover", ev => {
+        if (ev.target.className == "click-target") {
+            showPlayerColor(game.firstPlayer, ev.target);
+        }
+    })
+
+    clickTargets.addEventListener("mouseleave", ev => {
+        if (ev.target.className == "click-target") {
+            ev.target.setAttribute("class", "click-target");
+        }
+    })
 
     newGame.addEventListener('click', ev=>{
         game = new Game(player1.value,player2.value)
--- game.js
+++ game.js
@@ -2,6 +2,15 @@ export default class Game {
     constructor(player1,player2) {
         this.player1 = player1;
         this.player2=player2;
+        this.firstPlayer = 1;
+    }
+
+    playInColumn() {
+        if(this.firstPlayer == 1) {
+            this.firstPlayer = 2;
+        } else {
+            this.firstPlayer = 1;
+        }
     }
 
     getName() {
